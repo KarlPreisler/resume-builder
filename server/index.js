1990917c27e0be3c653b9f6a2d72b1386641d470
KarlPreisler <Karlpreisler@gmail.com>
2023-05-10 11:57:53 +0000

    Install and setup Multer

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(cors());
 
@@ -15,4 +18,19 @@ app.get("/api", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+});
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 },
 });
